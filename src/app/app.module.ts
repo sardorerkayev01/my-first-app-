b56a134779ea8bf14913d5c3ff356413fa9dc58a
sardorerkayev01 <sardorerkayev4214@gmail.com>
2023-10-15 22:03:07 +0500

    my first update

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { MaterialModule } from './shared/material/material.module';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    
+  ],
+
+
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    MatSlideToggleModule,
+    
+  ],
+  providers: [
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } },
+  
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
